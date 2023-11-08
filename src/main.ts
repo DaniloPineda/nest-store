ca07af55e0fcca5220169c4fce9864d742f15226
Danilo <danilopineda93@gmail.com>
2023-11-07 19:15:45 -0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // ignore unwanted properties
+    forbidNonWhitelisted: true, //block unwanted properties
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('DP Store')
+    .setDescription('This is a test project for NEST API creation')
+    .setVersion('1.0')
+    .build()
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, doc);
+
+  app.enableCors(); //confgure specific clients
+
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
